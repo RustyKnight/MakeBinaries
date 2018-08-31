acdc86833d8fea6eeaa9a52be81c9eaa070b7d75
Shane Whitehead <shane.whitehead@beamcommunications.com>
2018-08-31 12:56:15 +1000

    Initial Commit

---
+++ MakeBinaries/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MakeBinaries
+//
+//  Created by Shane Whitehead on 31/8/18.
+//  Copyright © 2018 KaiZen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
